4013eef4ce9654bae3ad3b9285b1c192c53022e7
Luke Mills <luke.m.mills@vanderbilt.edu>
2019-04-02 15:14:28 -0500

    tentative completeRequest

--- betting-app-api/completeRequest.js
+++ betting-app-api/completeRequest.js
@@ -15,6 +15,45 @@ export async function main(event, context) {
     }
   };
   //second call: modify balance
+  const params2 = {
+    TableName: "users",
+    // 'Key' defines the partition key and sort key of the item to be updated
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+    },
+    // 'UpdateExpression' defines the attributes to be updated
+    // 'ExpressionAttributeValues' defines the value in the update expression
+    UpdateExpression: "ADD balance = :val",
+    ExpressionAttributeValues: {
+      ":val": data.balance || 0,
+    },
+    // 'ReturnValues' specifies if and how to return the item's attributes,
+    // where ALL_NEW returns all attributes of the item after the update; you
+    // can inspect 'result' below to see how it works with different settings
+    ReturnValues: "ALL_NEW"
+  };
+
+  const params3 = {
+    TableName: "users",
+    // 'Key' defines the partition key and sort key of the item to be updated
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
+    Key: {
+      userId: data.otherUserId, //whatever this might be, make sure this and params2 aren't backwards
+    },
+    // 'UpdateExpression' defines the attributes to be updated
+    // 'ExpressionAttributeValues' defines the value in the update expression
+    UpdateExpression: "ADD balance = :val",
+    ExpressionAttributeValues: {
+      ":val": (-1 * data.balance) || 0,
+    },
+    // 'ReturnValues' specifies if and how to return the item's attributes,
+    // where ALL_NEW returns all attributes of the item after the update; you
+    // can inspect 'result' below to see how it works with different settings
+    ReturnValues: "ALL_NEW"
+  };
 
 
   //third call: add request to requesters array of past requests
@@ -25,6 +64,8 @@ export async function main(event, context) {
 
   try {
     const result = await dynamoDbLib.call("delete", params);
+    const result2 = await dynamoDbLib.call("update", params2);
+    const results3 = await dynamoDbLib.call("update", params3);
     return success({ status: true });
   } catch (e) {
     return failure({ status: e });
