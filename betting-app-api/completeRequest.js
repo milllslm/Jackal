f2cd9e075edf415f95f6d05cf892850cdef3a799
Luke Mills <luke.m.mills@vanderbilt.edu>
2019-04-09 16:39:44 -0500

    complete request now sets returnStatus to 2

--- betting-app-api/completeRequest.js
+++ betting-app-api/completeRequest.js
@@ -52,6 +52,27 @@ export async function main(event, context) {
     ReturnValues: "ALL_NEW"
   };
 
+  const params4 = {
+    TableName: "betting",
+    // 'Key' defines the partition key and sort key of the item to be updated
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
+    Key: {
+      userId: data.otherUserId,
+      betId: event.pathParameters.id
+    },
+    // 'UpdateExpression' defines the attributes to be updated
+    // 'ExpressionAttributeValues' defines the value in the update expression
+    UpdateExpression: "SET returningStatus = :returningStatus",
+    ExpressionAttributeValues: {
+      ":returningStatus": 2,
+    },
+    // 'ReturnValues' specifies if and how to return the item's attributes,
+    // where ALL_NEW returns all attributes of the item after the update; you
+    // can inspect 'result' below to see how it works with different settings
+    ReturnValues: "ALL_NEW"
+  };
+
 
   //third call: add request to requesters array of past requests
   //fourth call: add request to accepters array of past requests
@@ -63,6 +84,7 @@ export async function main(event, context) {
     const result = await dynamoDbLib.call("delete", params);
     const result2 = await dynamoDbLib.call("update", params2);
     const results3 = await dynamoDbLib.call("update", params3);
+    const results4 = await dynamoDbLib.call("update", params4);
     return success(result);
   } catch (e) {
     return failure({ status: e });
