fce35481fbeb24e7ccf556d6a4cabb50329cbca0
Luke Mills <luke.m.mills@vanderbilt.edu>
2019-04-09 16:32:06 -0500

    massive api update, fullfillment pipeline

--- betting-app-api/completeRequest.js
+++ betting-app-api/completeRequest.js
@@ -8,9 +8,8 @@ export async function main(event, context) {
     TableName: "betting",
     // 'Key' defines the partition key and sort key of the item to be removed
     // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'betId': path parameter
     Key: {
-      userId: event.requestContext.identity.cognitoIdentityId,
+      userId: data.otherUserId, //original card poster
       betId: data.betId
     }
   };
@@ -19,15 +18,14 @@ export async function main(event, context) {
     TableName: "users",
     // 'Key' defines the partition key and sort key of the item to be updated
     // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
     Key: {
-      userId: event.requestContext.identity.cognitoIdentityId,
+      userId: event.requestContext.identity.cognitoIdentityId, //fulfiller
     },
     // 'UpdateExpression' defines the attributes to be updated
     // 'ExpressionAttributeValues' defines the value in the update expression
-    UpdateExpression: "ADD balance = :val",
+    UpdateExpression: "SET balance = balance + :val",
     ExpressionAttributeValues: {
-      ":val": data.balance || 0,
+      ":val": parseInt(data.balance) || 0,
     },
     // 'ReturnValues' specifies if and how to return the item's attributes,
     // where ALL_NEW returns all attributes of the item after the update; you
@@ -39,15 +37,14 @@ export async function main(event, context) {
     TableName: "users",
     // 'Key' defines the partition key and sort key of the item to be updated
     // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
     Key: {
-      userId: data.otherUserId, //whatever this might be, make sure this and params2 aren't backwards
+      userId: data.otherUserId, //original card poster
     },
     // 'UpdateExpression' defines the attributes to be updated
     // 'ExpressionAttributeValues' defines the value in the update expression
-    UpdateExpression: "ADD balance = :val",
+    UpdateExpression: "SET balance = balance + :val",
     ExpressionAttributeValues: {
-      ":val": (-1 * data.balance) || 0,
+      ":val": (-1 * parseInt(data.balance)) || 0,
     },
     // 'ReturnValues' specifies if and how to return the item's attributes,
     // where ALL_NEW returns all attributes of the item after the update; you
@@ -66,7 +63,7 @@ export async function main(event, context) {
     const result = await dynamoDbLib.call("delete", params);
     const result2 = await dynamoDbLib.call("update", params2);
     const results3 = await dynamoDbLib.call("update", params3);
-    return success({ status: true });
+    return success(result);
   } catch (e) {
     return failure({ status: e });
   }
